0df257987c37236da7068e9cf90176ab5bcb34a3
AminG82 <mmnghasemi@gmail.com>
2026-09-08 16:41:39 +0330

    DBContext service Changed

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<LibraryDbContext>(options =>
-    options.UseSqlServer(
+    options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")
     ));
 
