13631beb8135b128f6c8602cb4079c4ffa3ef0b2
Amin Ghasemi <mmnghasemi@gmail.com>
2026-09-09 10:23:58 +0330

    BooksController.cs Beta Version Created!

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using LibraryManagement.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly LibraryDbContext _context;
+
+    public BooksController(LibraryDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var books = await _context.Books
+            .Include(b => b.Category)
+            .ToListAsync();
+
+        return Ok(books);
+    }
+}
