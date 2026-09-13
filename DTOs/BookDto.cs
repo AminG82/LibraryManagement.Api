e48331a8e958e35ab0ed8ca1d01b9b82d75b3c41
AminG82 <mmnghasemi@gmail.com>
2026-09-13 11:51:57 +0330

    BookController Get Api Coded and Json Serialization problem solved by creating DTOs-BookDto

---
+++ DTOs/BookDto.cs
@@ -0,0 +1,20 @@
+﻿namespace LibraryManagement.Api.DTOs;
+
+public class BookDto
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Author { get; set; } = string.Empty;
+
+    public string? ISBN { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = string.Empty;
+
+    public int TotalCount { get; set; }
+
+    public int AvailableCount { get; set; }
+}
