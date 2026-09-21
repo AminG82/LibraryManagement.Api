26a6c12e58ec0cdab59646ee5e3b7a9a788d2891
Amin Ghasemi <mmnghasemi@gmail.com>
2026-09-21 11:18:34 +0330

    CreateMemberDto and MemberDto Added

---
+++ DTOs/MemberDto.cs
@@ -0,0 +1,11 @@
+﻿namespace LibraryManagement.Api.DTOs;
+
+public class MemberDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string NationalCode { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+    public DateTime RegisterDate { get; set; }
+}
