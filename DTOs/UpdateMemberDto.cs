25d1e5202d9f1f24e0e46278c5bacd8f7a91e428
Amin Ghasemi <mmnghasemi@gmail.com>
2026-09-21 11:20:49 +0330

    UpdateMemberDto Added

---
+++ DTOs/UpdateMemberDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagement.Api.DTOs;
+
+public class UpdateMemberDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string NationalCode { get; set; } = string.Empty;
+    public string? Phone { get; set; }
+}
