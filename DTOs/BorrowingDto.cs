406fd776f8233f818168c5be7f56adde03266ba2
AminG82_Ubuntu <mmnghasemi@gmail.com>
2026-09-23 15:19:05 +0330

    BorrowingDto.cs Added

---
+++ DTOs/BorrowingDto.cs
@@ -0,0 +1,16 @@
+namespace LibraryManagement.Api.DTOs;
+
+public class BorrowingDto
+{
+    public int Id { get; set; }
+
+    public int BookId { get; set; }
+    public string BookTitle { get; set; } = string.Empty;
+
+    public int MemberId { get; set; }
+    public string MemberName { get; set; } = string.Empty;
+
+    public DateTime BorrowDate { get; set; }
+    public DateTime? ReturnDate { get; set; }
+    public bool IsReturned { get; set; }
+}
