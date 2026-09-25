c124cb27134af6f4631af17ed3cbb4c87829548d
AminG82 <mmnghasemi@gmail.com>
2026-09-25 15:37:34 +0330

    ReportsController.cs Added

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,44 @@
+﻿using LibraryManagement.Api.Data;
+using LibraryManagement.Api.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReportsController : ControllerBase
+{
+    private readonly LibraryDbContext _context;
+
+    public ReportsController(LibraryDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Reports/summary
+    [HttpGet("summary")]
+    public async Task<IActionResult> GetSummary()
+    {
+        var totalBooks = await _context.Books
+            .SumAsync(b => b.TotalCount);
+
+        var availableBooks = await _context.Books
+            .SumAsync(b => b.AvailableCount);
+
+        var totalMembers = await _context.Members
+            .CountAsync();
+
+        var borrowedBooks = totalBooks - availableBooks;
+
+        var result = new LibrarySummaryDto
+        {
+            TotalBooks = totalBooks,
+            AvailableBooks = availableBooks,
+            BorrowedBooks = borrowedBooks,
+            TotalMembers = totalMembers
+        };
+
+        return Ok(result);
+    }
+}
