94b4e40d6ec7cce5f50ba475b13f6f8e541f8741
AminG82 <mmnghasemi@gmail.com>
2026-09-25 16:33:16 +0330

    AddCORS added to program.cs

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ builder.Services.AddDbContext<LibraryDbContext>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
