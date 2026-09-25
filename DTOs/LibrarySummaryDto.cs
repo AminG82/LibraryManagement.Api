71e4592155a65ab9c8a64f886b7b9b30490c9360
AminG82 <mmnghasemi@gmail.com>
2026-09-25 15:36:02 +0330

    LibrarySummaryDto.cs Added

---
+++ DTOs/LibrarySummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace LibraryManagement.Api.DTOs;
+
+public class LibrarySummaryDto
+{
+    public int TotalBooks { get; set; }
+    public int AvailableBooks { get; set; }
+    public int BorrowedBooks { get; set; }
+    public int TotalMembers { get; set; }
+}
